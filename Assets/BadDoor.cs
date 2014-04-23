ed77e58ec5c580a6f02efe66df8555a34e1be667
Joan Magno <magnojoan@gmail.com>
2014-04-23 15:38:32 +0800

    Room with fire added

--- Assets/BadDoor.cs
+++ Assets/BadDoor.cs
@@ -2,14 +2,59 @@
 using System.Collections;
 
 public class BadDoor : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	
+	// Smothly open a door
+	float smooth = 2.0f;
+	float DoorOpenAngle = 90.0f;
+	private bool open;
+	private bool enter;
+	
+	private Vector3 defaultRot;
+	private Vector3 openRot;
+
+	public AudioClip ouchHotDoorAudio;
+
+	
+	void Start() {
+		defaultRot = transform.eulerAngles;
+		openRot = new Vector3 (defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
 	}
 	
-	// Update is called once per frame
+	//Main function
 	void Update () {
+		if(open){
+			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+
+		} else{
+			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+		}
+		// if(Input.GetKeyDown("f") && enter){
+		if( enter && MainControls.Check ( MasterController.Key.OpenDoor ) ){
+			open = !open;
+			LevelCountdown.AddTime(-5);
+			AudioSource.PlayClipAtPoint(ouchHotDoorAudio, transform.position, 1); 
+		}
+	}
 	
+	void OnGUI() {
+		if(enter){
+			GUI.Label(new Rect(Screen.width/2 - 75, Screen.height - 100, 150, 30), "Press A (key F) to open the door");
+		}
+	}
+	
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider other){
+		// Debug.Log (other.gameObject.name);
+		if (other.gameObject.tag == "Player") {
+			enter = true;
+		}
+	}
+	
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player") {
+			enter = false;
+		}
 	}
 }
