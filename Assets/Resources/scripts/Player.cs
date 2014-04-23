9216c3fafa69ddab421fd7a7f3be55918feb7074
Tim Joseph Dumol <tim@timdumol.com>
2014-04-23 17:33:12 +0800

    Add fire indicator.

--- Assets/Resources/scripts/Player.cs
+++ Assets/Resources/scripts/Player.cs
@@ -3,22 +3,57 @@ using System.Collections;
 using System;
 
 public class Player : MonoBehaviour {
-	//CONSTANTS
-	const float SPEED = 6;
+    //CONSTANTS
+    const float SPEED = 6;
+    const float FIRE_THRESHOLD = 0.5f;
+    const float SHAKE_THRESHOLD = 15f;
+    const float SHAKE_TIMES_THRESHOLD = 4f;
+    
+    static float fireCounter = 0f;
+    static float shakeCounter = 0f;
+    static Quaternion lastRotation = new Quaternion ();
 
-	//SOUNDS
-	public AudioClip successAudio;
-	public AudioClip gameOverAudio;
-	public AudioClip nextLevelAudio;
+    public static void Reset() {
+        fireCounter = shakeCounter = 0f;
+        lastRotation = new Quaternion ();
+    }
 
-	public AudioClip do_not_use_the_elevator;
-	public AudioClip picked_up_a_first_aid_kit;
-	public AudioClip protected_yourself;
+    void FixedUpdate() {
+        if (fireCounter >= FIRE_THRESHOLD) {
+            // Trigger fire damage
+            LevelCountdown.AddTime(-5e-2f);
+            
+            // Handle shaking mechanism
+            Quaternion rot = transform.Find ("Main Camera").gameObject.transform.rotation; // Change Main Camera to OVRCameraController for rift.
+            float angle = Mathf.Abs(Quaternion.Angle(rot,lastRotation));
+            if (angle >= SHAKE_THRESHOLD) {
+                shakeCounter += 1;
+                if (shakeCounter >= SHAKE_TIMES_THRESHOLD) {
+                    fireCounter = 0f;
+                    shakeCounter = 0f;
+                }
+            }
+            lastRotation = rot;
+        }
+    } 
+
+    
+    //SOUNDS
+    public AudioClip successAudio;
+    public AudioClip gameOverAudio;
+    public AudioClip nextLevelAudio;
+    
+    public AudioClip do_not_use_the_elevator;
+    public AudioClip picked_up_a_first_aid_kit;
+    public AudioClip protected_yourself;
 	public AudioClip stay_away_from_fires;
 	public AudioClip stay_away_from_windows;
 	public AudioClip you_have_been_crushed_by_falling;
 
+    private GameObject fireIndicator;
+
 	private bool isCrawling = false;
+    private GameObject fireIndicatorInstance;
 
 	void OnCollisionEnter(Collision collision) {
 		//GameObject obj = collision.gameObject;
@@ -80,14 +115,35 @@ public class Player : MonoBehaviour {
 			lastFire = now;
 
 			MasterController.ShowMessage("");
-			LevelCountdown.AddTime(-1f);
-		} 
+			LevelCountdown.AddTime(-0.4f);
+            fireCounter += 1;
+		}
 	}
 
 	void Start(){
+        fireIndicator = Resources.Load("prefab/FireIndicator") as GameObject;
 	}
 
 	void Update () {
+        if (fireCounter >= FIRE_THRESHOLD) {
+            MasterController.ShowMessage("You are on fire! Get out of danger, then stop, drop, and roll! (Shake your head!)");
+
+            Transform cameraTransform = transform.Find ("Main Camera").gameObject.transform; // Change Main Camera to OVRCameraController for rift.
+            // Materialize the fire indicator thing
+            Vector3 indicatorPos = cameraTransform.position + cameraTransform.forward - 0.5f*cameraTransform.up;
+            Quaternion indicatorRot = cameraTransform.rotation;
+            if (fireIndicatorInstance == null) {
+                fireIndicatorInstance = Instantiate(fireIndicator, indicatorPos, indicatorRot) as GameObject;
+            } else {
+                fireIndicatorInstance.transform.position = indicatorPos;
+                fireIndicatorInstance.transform.rotation = indicatorRot;
+            }
+        } else {
+            if (fireIndicatorInstance != null) {
+                Destroy(fireIndicatorInstance);
+                fireIndicatorInstance = null;
+            }
+        }
 
 		if(Input.GetKeyDown("c") && isCrawling){
 			// this.transform.position += Vector3.up;
